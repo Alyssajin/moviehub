b74caa997b957ff754304edb428b57d45aa71103
alyssajin <alyssajin2019@gmail.com>
2026-01-28 10:18:08 -0800

    feat: enhance tab navigation with custom icons for Home, Search, Saved, and Profile

--- moivehub/app/(tabs)/_layout.tsx
+++ moivehub/app/(tabs)/_layout.tsx
@@ -1,14 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, ImageBackground, Image } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
 const _layout = () => {
     return (
         <Tabs>
-            <Tabs.Screen name="index" options={{ headerShown: false, title: 'Home' }} />
-            <Tabs.Screen name="search" options={{ headerShown: false, title: 'Search' }} />
-            <Tabs.Screen name="saved" options={{ headerShown: false, title: 'Saved' }} />
-            <Tabs.Screen name="profile" options={{ headerShown: false, title: 'Profile' }} />
+            <Tabs.Screen name="index" options={{
+                headerShown: false,
+                title: 'Home',
+                tabBarIcon: ({ focused }) => (
+                    <>
+                        <ImageBackground
+                            source={images.highlight}
+                            className='flex flex-row w-full flex-1 min-w-[112px] 
+                            min-h-14 mt-4 justify-center items-center 
+                            rounded-full overflow-hidden'>
+                            <Image source={icons.home} style={{ width: 20, height: 20, tintColor: '#151312' }} />
+                            <Text className='text-secondary text-base font-semibold ml-2'> HOME </Text>
+                        </ImageBackground>
+                    </>
+                )
+            }} />
+            <Tabs.Screen name="search" options={{ headerShown: false, title: 'Search', tabBarIcon: ({ focused }) => <Text className={focused ? 'text-primary' : 'text-light-100'}>Search</Text> }} />
+            <Tabs.Screen name="saved" options={{ headerShown: false, title: 'Saved', tabBarIcon: ({ focused }) => <Text className={focused ? 'text-primary' : 'text-light-100'}>Saved</Text> }} />
+            <Tabs.Screen name="profile" options={{ headerShown: false, title: 'Profile', tabBarIcon: ({ focused }) => <Text className={focused ? 'text-primary' : 'text-light-100'}>Profile</Text> }} />
         </Tabs>
     )
 }
